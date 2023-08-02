3a154ac23c11a6ebfa292edaa3ddf1c7ce3f0e4c
Shrirang <shrirangthatsit10@gmail.com>
2023-08-03 00:29:31 +0530

    started delete method

--- server.py
+++ server.py
@@ -25,34 +25,56 @@ def date():
     conversation = 'Date: ' + datenow
     return conversation
 
+def handle_delete_request(client,clientport,resource): 
+    #if present then delete and send status code 204
+    #check if resource is present
+    #if resource is /database.txt/123 where 123 is id
+    #extract id from resource
 
+    #delete resource
+    print(resource)
+    index=resource.find('database.txt/')
+    if(index == -1):
+        #call status code error
+        print("Not found")
+        return
+    id=resource[index+13:]
+    print(id)
+    with open("./database.txt",'r') as file:
+        lines=file.readlines()
+    count=len(lines)
+    with open("database.txt",'r') as file:    
+        for line in file:
+            row=line.split(' ')
+            if(row[0] == id):
+                lines.remove(line)
+                break
+    if(len(lines) == count):
+        print("Not found")
+        #call status code error
+        exit        
+    else:
+        with open("database.txt",'w') as file:
+            file.writelines(lines)
+    text='\r\nHTTP/1.1 204 No Content'
+    client.send(text.encode())
 def handle_get_request(client, port,entity,serverport):
     isItFile = os.path.isfile(entity)
     isItDir  = os.path.isdir(entity)
-    text = 'HTTP/1.1 200 OK'
     ip = '127.0.0.1'
-    text += '\r\n Connection: keep-alive'
-    text += '\r\n Content-Language: en-US'
-    text += '\r\n Server: ' + ip
-    text += '\r\nStrict-Transport-Security: max-age=63072000'
-    text += '\r\nX-Content-Type-Options: nosniff'
-    text += '\r\nX-Frame-Options: DENY'
-    text += '\r\nX-XSS-Protection: 1; mode=block'
-    # Adding data
+    textdata=''
     if(isItDir):
-        text += '\r\nContent-type: text/html; charset=utf-8'
-        text += '\r\n\r\n'
-        text += '\r\n<!DOCTYPE html>'
-        text += '\r\n<html lang="en">'
-        text += '\r\n<head>'
-        text += '\r\n<meta charset="utf-8">'
-        text += '\r\n<title>A simple webpage</title>'
-        text += '\r\n</head>'
-        text += '\r\n<body>'
-        text += '\r\n<h1>Current Directory </h1>'
-        dir_list = os.listdir(entity)
-        text+="\r\n<ul>"    
-        for line in dir_list:
+        textdata += '\r\n<!DOCTYPE html>'
+        textdata += '\r\n<html lang="en">'
+        textdata += '\r\n<head>'
+        textdata += '\r\n<meta charset="utf-8">'
+        textdata += '\r\n<title>A simple webpage</title>'
+        textdata += '\r\n</head>'
+        textdata += '\r\n<body>'
+        textdata += '\r\n<h1>Current Directory </h1>'
+        dir_list1 = os.listdir(entity)
+        textdata+="\r\n<ul>"    
+        for line in dir_list1:
             if entity == '/':
                 # link = 'http://' + ip + ':' + str(serverport) + entity + '/'+ line
                 l = '\r\n<li><a href ="'+link+'">'+line+'</a></li>'
@@ -60,25 +82,42 @@ def handle_get_request(client, port,entity,serverport):
             else:
                 link = 'http://' + ip + ':' + str(serverport) + entity + '/'+ line
                 l = '\r\n<li><a href ="'+link+'">'+line+'</a></li>'
-                text += l
-        text+="\r\n</ul>"
-        text+="\r\n<br>"
-        text+="\r\n<h2>Post Request Form</h2>"
-        text+=read_file_contents("./form.html")
-        text += '\r\n</body>'
-        text += '\r\n</html>'
-        print("Sending response...")
-        client.send(text.encode())
+                textdata += l
+        textdata+="\r\n</ul>"
+        textdata+="\r\n<br>"
+        textdata+="\r\n<h2>Post Request Form</h2>"
+        textdata+=read_file_contents("./form.html")
+        textdata += '\r\n</body>'
+        textdata += '\r\n</html>'
+        dir_data_length=len(textdata)   #Length of data to be sent if a directory
+    text = '\r\nHTTP/1.1 200 OK'
+    text += '\r\nConnection: keep-alive'
+    text += '\r\nContent-Language: en-US'
+    text += '\r\nServer: ' + ip
+    text += '\r\nStrict-Transport-Security: max-age=63072000'
+    text += '\r\nX-Content-Type-Options: nosniff'
+    text += '\r\nX-Frame-Options: DENY'
+    text += '\r\nX-XSS-Protection: 1; mode=block'
+    # Adding data
+    if(isItDir):
+        text += '\r\nContent-type: text/html; charset=utf-8'
+        text+='\r\nContent-length: '+str(dir_data_length)
+        text += '\r\n\r\n'
+        text+=textdata
+        encoded=text.encode()
+        client.send(encoded)
+        print("Response Sent...",port)
     elif(isItFile):
         size = os.path.getsize(entity)
-        text+='\r\Content-type: text/plain'
+        text+='\r\n\Content-type: text/plain'
         text+='\r\nContent-length: '+str(size)
         text += '\r\n\r\n'
         try:            
-            print("Sending response...")
             f = open(entity, "rb")
-            client.send(text.encode())
+            encoded=text.encode()
+            client.send(encoded)
             client.sendfile(f)
+            print("Response Sent... ",port)
         except:
             pass
         return
@@ -92,17 +131,21 @@ def handle_post_request(client,clientport,entity,headers,post_data):
         if(content_type != 'application/x-www-form-urlencoded'):
             #some status code 
             pass
-        #Process & Extract Post method data 
+        #Process & Extract Post method data
+        print(post_data) 
         index1= post_data.find("name=")
-        index2= post_data.find("&")
+        index2= post_data.find("&email")
         Name =post_data[index1+5:index2]
+        index3=post_data.find("mis=")
+        index4=post_data.find("&name")
+        Id=post_data[index3+4 :index4]
         index1=post_data.find("email=")
         Email=post_data[index1+6:]
 
         #Write the processed data to database.txt
         f =open("database.txt","+a")
         f.write("\n")
-        f.write(Name + "    " + Email)
+        f.write(Id + "  "+ Name + " " + Email)
         text = 'HTTP/1.1 200 OK'
         ip = '127.0.0.1'
         text += '\r\n Connection: keep-alive'
@@ -144,7 +187,8 @@ def client_request_handler(server, client, port,serverport):
                 handle_get_request(client, port,entity,serverport)
             elif method == 'POST':
                 handle_post_request(client, port,entity,headers,post_data)
-
+            elif method == 'DELETE':
+                handle_delete_request(client,port,entity)
             if not data:
                 print("IF NOT DATA!!!")
                 break
@@ -157,7 +201,7 @@ def client_request_handler(server, client, port,serverport):
 
 def main():
     HOST = '127.0.0.1'
-    PORT = 8120
+    PORT = 8132
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen(5)
